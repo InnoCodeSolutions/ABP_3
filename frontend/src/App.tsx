8a0c4830b5d4bd476769296992d3166fcd6e334e
unknown <samuellucasvieira.melo@gmail.com>
2024-09-16 13:07:14 -0300

    Atualização de funcionalidade login e cadastro aos cors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,26 @@
-import Home from "./pages/Home";
-import LoginPage from "./pages/Login";
-import CadastroPage from "./pages/Cadastro";
-import AddAlimentoPage from "./pages/AddAlimento";
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/Login';
+import Home from './pages/Home';
+import CadastroPage from './pages/Cadastro';
+import AddAlimentoPage from './pages/AddAlimento';
+import LoginForm from './components/LoginForm/LoginForm';
+import Login from './pages/Login';
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <div>
-      <AddAlimentoPage/>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/cadastro" element={<CadastroPage/>} />
+        <Route path="/Alimento" element={<AddAlimentoPage/>} />
+        {/* Adicione outras rotas aqui */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
