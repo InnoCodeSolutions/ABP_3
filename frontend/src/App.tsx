f42112d8b8a4edb3ecb3d9b51806644c599c011a
Mauro <mauroprado1227@gmail.com>
2024-09-16 20:08:45 -0300

    cadastrarRefeicao

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import CadastroPage from './pages/Cadastro';
 import AddAlimentoPage from './pages/AddAlimento';
 import { AuthProvider, useAuth } from './context/AuthContext';
+import NovaRefeicao from './pages/NovaRefeicao';
 
 
 const ProtectedRoute: React.FC<{ element: JSX.Element }> = ({ element }) => {
@@ -21,7 +22,8 @@ const App: React.FC = () => {
           <Route path="/" element={<Navigate to="/login" />} /> {/* Redireciona a rota padrão para /login */}
           <Route path="/cadastro" element={<CadastroPage />} />
           <Route path="/alimento" element={<ProtectedRoute element={<AddAlimentoPage />} />} />
-          <Route path="/Homes" element={<ProtectedRoute element={<Home />} />} />
+          <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
+          <Route path="/novarefeicao" element={<NovaRefeicao/>} />
           {/* Adicione outras rotas aqui */}
         </Routes>
       </Router>
