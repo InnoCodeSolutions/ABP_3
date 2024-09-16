8aef1fcbf31b04fda0527aa0dcb3d3e9bce95b72
andreflavio <13demaio198179@gmail.com>
2024-09-16 18:42:39 -0300

    arrumando coisas básicas

--- backend/src/models/Alimento.ts
+++ backend/src/models/Alimento.ts
@@ -1,25 +1,3 @@
-/*import mongoose, { Schema, Document } from 'mongoose';
-
-interface IAlimento extends Document {
-  nome: string;
-  energia: number;
-  proteina: number;
-  lipideos: number;
-  carboidratos: number;
-  fibra: number;
-}
-
-const AlimentoSchema: Schema = new Schema({
-  nome: { type: String, required: true },
-  energia: { type: Number, required: true },
-  proteina: { type: Number, required: true },
-  lipideos: { type: Number, required: true },
-  carboidratos: { type: Number, required: true },
-  fibra: { type: Number, required: true }
-});
-
-export const Alimento = mongoose.model<IAlimento>('Alimento', AlimentoSchema);
-*/
 import { Schema, model, Document } from 'mongoose';
 
 interface IAlimento extends Document {
