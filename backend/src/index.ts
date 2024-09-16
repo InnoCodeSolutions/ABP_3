3253355a3b1293a6ece71c4d87f0e96232c3c92f
unknown <samuellucasvieira.melo@gmail.com>
2024-09-16 11:24:11 -0300

    Inserção do cors no backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import routes from './routes';
 import dotenv from 'dotenv';
+import cors from "cors";
+import bodyParser from 'body-parser';
 import connect from './models/connection';
 
 dotenv.config();
@@ -11,14 +13,15 @@ const app = express(); // Cria o servidor e coloca na variável app
 
 // Suportar parâmetros JSON no body da requisição
 app.use(express.json());
-
+app.use(cors());
+app.use(bodyParser.json());
 // Conecta ao MongoDB no início da aplicação
 connect();
-
-// Define a rota para o pacote /routes
-app.use(routes);
 
 // Inicializa o servidor na porta especificada
 app.listen(PORT, () => {
     console.log(`Rodando na porta ${PORT}`);
 });
+
+// Define a rota para o pacote /routes
+app.use(routes);
